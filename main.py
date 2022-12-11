4d83e6e854cd246a8f2f08cf39cbc5b38c5da543
liutristan <113070063+liutristan@users.noreply.github.com>
2022-12-11 15:31:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,27 @@ import os
 import random
 import requests
 import threading
-
-
-class stats():
-    hits = 0
-    failed = 0
+from numerize import numerize
+from colorama import Fore, Back, Style
+os.system(f'title Shuttle Network dsc.gg Click Bot')
+class a():
+    s = 0
+    f = 0
 
 
 os.system("cls")
-threads = int(input('Threads --'))
+print(Back.RED + Fore.BLACK + '''█████████████████████████████████████████████████████████████
+█─▄▄▄▄█─█─█▄─██─▄█─▄─▄─█─▄─▄─█▄─▄███▄─▄▄─███▄─▄─▀█─▄▄─█─▄─▄─█
+█▄▄▄▄─█─▄─██─██─████─█████─████─██▀██─▄█▀████─▄─▀█─██─███─███
+▀▄▄▄▄▄▀▄▀▄▀▀▄▄▄▄▀▀▀▄▄▄▀▀▀▄▄▄▀▀▄▄▄▄▄▀▄▄▄▄▄▀▀▀▄▄▄▄▀▀▄▄▄▄▀▀▄▄▄▀▀\n\n\n''' + Style.RESET_ALL)
+welcome = input(Fore.GREEN + "Welcome to Shuttle dsc.gg Bot! Press Enter to continue" + Style.RESET_ALL)
+threads = int(input(Fore.RED + "\n" * 5 + "Threads --> " + Fore.GREEN))
+code = input(Fore.RED + "Code to bot --> " + Fore.GREEN)
 
 
 def click():
     try:
-        os.system(f"title discord.gg/xl ^| Hits: {stats.hits} ^| Failed {stats.failed}")
+        os.system(f"title discord.gg/xl ^| shuttle.cf ^| Success: {numerize.numerize(a.s)} ^| Failed {numerize.numerize(a.f)}")
 
         proxys = open('proxies.txt', 'r').read().splitlines()
         proxy = random.choice(proxys)
@@ -26,10 +33,10 @@ def click():
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
         }
 
-        r = requests.get(f"https://dsc.gg/{code}", headers=headers, proxies=proxies)
-        stats.hits += 1
+        r = requests.get(f"https://dsc.gg/{code}?ref=homepage", headers=headers, proxies=proxies)
+        a.s += 1
     except:
-        stats.failed += 1
+        a.f += 1
 
 while True:
     if threading.active_count() < threads:
